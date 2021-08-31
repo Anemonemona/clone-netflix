73f629b6e70e2acd79f9a8950e17baafa467ce76
Anemonemona <nohannalacerda@hotmail.com>
2021-08-31 16:09:38 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Clone página inicial Netflix
---
+++ js/botoes.js
@@ -0,0 +1,7 @@
+function assistir(){
+    window.location.href = "https://www.youtube.com/watch?v=vZ734NWnAHA&ab_channel=StarWars"
+}
+
+function info(){
+    window.location.href = "https://starwars.fandom.com/pt/wiki/Star_Wars_Epis%C3%B3dio_IV:_Uma_Nova_Esperan%C3%A7a"
+}
